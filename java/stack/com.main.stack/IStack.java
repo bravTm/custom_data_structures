b8678f6f4f7f8ed6129b5aeae1d61a6044a5dd33
Evgeny <98524775+bravTm@users.noreply.github.com>
2023-09-18 18:10:31 +0300

    Create IStack.java

---
+++ java/stack/com.main.stack/IStack.java
@@ -0,0 +1,11 @@
+package com.main.stack;
+
+public interface IStack {
+
+    public void push(float value);
+    public float pop();
+    public float top();
+    public int size();
+    public void printing();
+
+}
